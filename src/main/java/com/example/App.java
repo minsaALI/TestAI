86991bb657fbc2f3f366e2b5a969fb89e90a72c6
Mujahid Ali <mails4mujahidali@gmail.com>
2025-07-05 12:50:37 +0530

    initial push

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        // Unused variable (Sonar issue)
+        int unused = 0;
+
+        // Duplicate string literals (Sonar issue)
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        // System.out used (Sonar issue)
+        System.out.println("Using system out");
+
+        // Cognitive complexity (Sonar issue)
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                if (i % 3 == 0) {
+                    if (i % 5 == 0) {
+                        System.out.println("FizzBuzz");
+                    }
+                }
+            }
+        }
+    }
+}

a21d6288a2471d25b4e0e8527b7d307be0b88091
Mujahid Ali <mails4mujahidali@gmail.com>
2025-07-05 14:51:10 +0530

    1 issue

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,28 +2,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        // Unused variable (Sonar issue)
-        int unused = 0;
+
         String a=null;
         String b="Hi";
-        String hardcodedPassword = "admin123";
         System.out.println(a.equals(b));
-        // Duplicate string literals (Sonar issue)
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
-        // System.out used (Sonar issue)
-        System.out.println("Using system out");
-
-        // Cognitive complexity (Sonar issue)
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                if (i % 3 == 0) {
-                    if (i % 5 == 0) {
-                        System.out.println("FizzBuzz");
-                    }
-                }
-            }
-        }
+        
     }
 }
